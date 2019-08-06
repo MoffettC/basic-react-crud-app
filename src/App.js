acb1eba59d77ca7668439b058619e66735941f97
Chris Moffett <chris.moffett@moesol.com>
2019-08-06 09:53:20 -0700

    Add update form/effect hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import UserTable from './tables/UserTable'
 import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
 
 const App = () => {
   const usersData = [
@@ -9,7 +10,11 @@ const App = () => {
     { id: 3, name: 'Ben', username: 'benisphere' },
   ]
 
-  const [users, setUsers] = useState(usersData)
+  const initialFormState = { id: null, name: '', username: '' }
+
+  const [users, setUsers] = useState(usersData) //array destructuring
+  const [editing, setEditing] = useState(false)
+  const [currentUser, setCurrentUser] = useState(initialFormState)
 
   const addUser = user => {
     user.id = users.length + 1
@@ -17,7 +22,18 @@ const App = () => {
   }
 
   const deleteUser = id => {
+    setEditing(false)
     setUsers(users.filter(user => user.id !== id))
+  }
+
+  const editRow = user => {
+    setEditing(true)
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  }
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false)
+    setUsers(users.map(user => (user.id === id ? updatedUser : user)))
   }
 
   return (
@@ -25,12 +41,26 @@ const App = () => {
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser} />
+          {editing ? (
+            <div>
+              <h2>Edit user</h2>
+              <EditUserForm
+                editing={editing}
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </div>
+          )}
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable users={users} deleteUser={deleteUser}/>
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
         </div>
       </div>
     </div>
