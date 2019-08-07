46196898841d6756b48ba0295ed54f6f8eac6310
Chris Moffett <20ninjas@gmail.com>
2019-08-07 07:56:21 -0700

    Adjust folder struct

---
+++ client/build/precache-manifest.97e7008bd36cbd983bd4f7580e9153a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cfc4b05a2b8104e8f9b8d90cdf0dcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cb0ff0f7da736bada7c",
+    "url": "/static/css/main.c5bf4cd2.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "6cb0ff0f7da736bada7c",
+    "url": "/static/js/main.6e8592de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
